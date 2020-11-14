1645e1502c9362c3a616c15b0ea49a1eddafe5aa
Gyuri Park <70451816+gyuripie@users.noreply.github.com>
2020-11-14 10:06:30 -0500

    Add files via upload

---
+++ Mock-EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        //print(sender.currentTitle)
+        timer.invalidate()
+        let hardness = sender.currentTitle
+        
+        secondsRemaining = eggTimes[hardness!]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        progressBar.progress = 1.0
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            
+   
+            
+        }
+    }
+}
